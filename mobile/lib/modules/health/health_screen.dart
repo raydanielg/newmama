4ac096626b74b934719e1bbaf322abe598250754
raydanielg <airezra2+1@gmail.com>
2026-05-02 12:13:32 +0300

    commit

---
+++ mobile/lib/modules/health/health_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HealthScreen extends StatelessWidget {
+  const HealthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Health Tracking')),
+    );
+  }
+}
