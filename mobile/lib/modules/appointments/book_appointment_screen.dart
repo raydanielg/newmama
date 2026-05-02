05f5dc9baa58ab5541087d4e2a29ad383997cf54
raydanielg <airezra2+1@gmail.com>
2026-05-02 12:15:04 +0300

    commit

---
+++ mobile/lib/modules/appointments/book_appointment_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BookAppointmentScreen extends StatelessWidget {
+  const BookAppointmentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Book Appointment')),
+    );
+  }
+}
---
+++ mobile/lib/modules/health/kick_count_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class KickCountScreen extends StatelessWidget {
+  const KickCountScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Kick Count Tracker')),
+    );
+  }
+}
