aa56686f073a6071b239237de6dd70f1852178cd
raydanielg <airezra2+1@gmail.com>
2026-07-27 21:21:26 +0300

    commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * Root entry point that forwards to the public directory.
+ * This is useful for shared hosting where the document root
+ * cannot be set to the public/ directory.
+ */
+
+require __DIR__ . '/public/index.php';
